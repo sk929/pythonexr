ac3a7faf28b96b30fcda9f66286533172df536bc
sk929 <kumarsaurav929@gmail.com>
2020-05-22 19:26:11 +0530

    Create Dict_Set.py

---
+++ Python_exr/Dict_Set.py
@@ -0,0 +1,15 @@
+planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune']
+planet_to_initial = {planet: planet[0] for planet in planets}
+print(planet_to_initial)
+'''{'Mercury': 'M',
+ 'Venus': 'V',
+ 'Earth': 'E',
+ 'Mars': 'M',
+ 'Jupiter': 'J',
+ 'Saturn': 'S',
+ 'Uranus': 'U',
+ 'Neptune': 'N'}'''
+ 
+ 
+ #Set No repeated value set1 = {1, 2, 3,5}  num = se
+ #Dict key Pair value   num = {1:2, 23: 67}
